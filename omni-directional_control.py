ff18dd96dbe921ac150f5fee4cb936ee0e538f4e
Yasuyuki Fujii <is0232xf@gmail.com>
2021-02-09 16:57:27 +0900

    [fix] bugs

--- omni-directional_control.py
+++ omni-directional_control.py
@@ -19,8 +19,15 @@ from INA226 import INA226
 
 const = parameter()
 
+control_mode = const.control_mode
+if control_mode == 0:
+    print('CONTROL MODE: OMNIDIRECTIONAL CONTROL')
+elif control_mode == 1:
+    print('CONTROL MODE: DIAGONAL CONTROL')
+
 # read waypoint file (csv)
 target_point_file = const.way_point_file
+print('WAY POINT FILE: ', target_point_file)
 target_point = np.genfromtxt(target_point_file,
                           delimiter=',',
                           dtype='float',
@@ -52,7 +59,7 @@ def set_arm_disarm(msg):
         cmd = 1
     elif(msg=='DISARM'):
         cmd = 0
-    ############################################################## Arm
+
     master.mav.command_long_send(master.target_system,
     master.target_component,
     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
@@ -97,26 +104,21 @@ def calc_heading_diff(way_point):
                   - math.sin(t_lat)*math.cos(c_lat)*math.cos(d_lon), math.sin(d_lon)*math.cos(c_lat)))
     return diff_heading
 
-def decide_next_action(pose, start_time, distace_tolerance, heading_torelance):
-    # decide the next action from current robot status and the next waypoint
-    current_point = np.array([pose[1], pose[0]])
-    current_yaw = pose[2]
-
-    diff_distance = round(mpu.haversine_distance(current_point, BIWAKO.next_goal), 5)*1000
-    # check distance between current and target
-    if abs(diff_distance) < distace_tolerance:
-        action = actions.stay_action()
-    # when the device has not received
-    else:
-        target_direction = math.radians(calculator.calculate_bearing(current_point, BIWAKO.next_goal))
-        diff_deg =  math.degrees(calculator.limit_angle(target_direction - current_yaw))
-        action = actions.omni_control_action(diff_deg, diff_distance)
-    return action
-
 def kill_signal_process(arg1, args2):
     pass
 
 def logging(arg1, args2):
+    update_robot_state()
+    v = power_sensor.get_voltage()
+    c = power_sensor.get_current()
+    p = power_sensor.get_power()
+    unix_time = time.time()
+    BIWAKO.count = BIWAKO.count + 0.1
+    data = [unix_time, BIWAKO.count, BIWAKO.lat, BIWAKO.lon, math.degrees(BIWAKO.yaw),
+            BIWAKO.cmd, BIWAKO.pwm, v, c, p]
+    log_data.append(data)
+
+def logging_with_wt(arg1, args2):
     update_robot_state()
     wt = wt_sensor.wt
     v = power_sensor.get_voltage()
@@ -151,42 +153,63 @@ time.sleep(7)
 master.arducopter_arm()
 print("Arm/Disarm: Arm")
 
-BIWAKO = Robot(target_point)
-wt_sensor = WT_sensor()
-addr = 0x40
-power_sensor = INA226(addr)
-power_sensor.initial_operation()
-
-update_wt_thread = threading.Thread(target=update_wt)
-update_wt_thread.start()
-
-log_data = []
-
-############ start to measure the second ############
-start_time = 0.0
 
 if __name__ == '__main__':
 
     state_data_log = const.data_log_mode
     debug_mode = const.debug_mode
+    wt_log_mode = const.wt_log_mode
+
+    control_mode = const.control_mode
+    if control_mode == 0:
+        print('CONTROL MODE: OMNIDIRECTIONAL CONTROL')
+    elif control_mode == 1:
+        print('CONTROL MODE: DIAGONAL CONTROL')
 
     main_target_distance_torelance = const.main_target_distance_torelance
     temp_target_distance_torelance = const.temp_target_distance_torelance
     heading_torelance = const.heading_torelance
     keep_time = const.duration
 
-    if (state_data_log==True):
+    BIWAKO = Robot(target_point)
+    addr = 0x40
+    power_sensor = INA226(addr)
+    power_sensor.initial_operation()
+
+    if wt_log_mode is True:
+        wt_sensor = WT_sensor()
+        update_wt_thread = threading.Thread(target=update_wt)
+        update_wt_thread.start()
+
+    log_data = []
+    deg_e = [0]
+    is_first = 0
+
+    ############ start to measure the second ############
+    start_time = 0.0
+
+    if (state_data_log is True):
         # get date time object
         detail = datetime.datetime.now()
         date = detail.strftime("%Y%m%d%H%M%S")
         # open csv file
         file = open('./csv/'+ date +'.csv', 'a', newline='')
         csvWriter = csv.writer(file)
-        csvWriter.writerow(['time', 'count', 'latitude', 'longitude', 'yaw', 'cmd',
-                            'pwm', 'wt', 'voltage', 'current', 'power_consumption'])
+        if wt_log_mode is True:
+            data_items = ['time', 'count', 'latitude', 'longitude', 'yaw', 'cmd',
+                          'pwm', 'wt', 'voltage', 'current', 'power_consumption']
+        elif wt_log_mode is False:
+            data_items = ['time', 'count', 'latitude', 'longitude', 'yaw', 'cmd',
+                          'pwm', 'voltage', 'current', 'power_consumption']
+
+        csvWriter.writerow(data_items)
 
     try:
-        signal.signal(signal.SIGALRM, logging)
+        if wt_log_mode is True:
+            signal.signal(signal.SIGALRM, logging_with_wt)
+        elif wt_log_mode is False:
+            signal.signal(signal.SIGALRM, logging)
+
         signal.setitimer(signal.ITIMER_REAL, 0.5, 0.5)
         while True:
             pose = [BIWAKO.lon, BIWAKO.lat, BIWAKO.yaw]
@@ -196,6 +219,7 @@ if __name__ == '__main__':
             diff_distance = round(mpu.haversine_distance(current_point, BIWAKO.next_goal), 5)*1000
 
             if abs(diff_distance) < main_target_distance_torelance:
+                is_first = 1
                 action = actions.stay_action()
                 BIWAKO.cmd = action[0]
                 BIWAKO.pwm = action[1]
@@ -203,9 +227,13 @@ if __name__ == '__main__':
                 time.sleep(0.02)
 
             else:
-                BIWAKO.temp_goal = calc_temp_target(current_point, BIWAKO.next_goal)
-                while True:
+                if is_first is 0:
+                    BIWAKO.temp_goal = BIWAKO.next_goal
+                elif is_first is 1:
+                    BIWAKO.temp_goal = calc_temp_target(current_point, BIWAKO.next_goal)
+                while abs(diff_distance) > temp_target_distance_torelance:
                     pose = [BIWAKO.lat, BIWAKO.lon, BIWAKO.yaw]
+
                     # decide the next action from current robot status and the next waypoint
                     current_point = np.array([pose[0], pose[1]])
                     current_yaw = pose[2]
@@ -214,33 +242,43 @@ if __name__ == '__main__':
                     diff_distance = round(mpu.haversine_distance(current_point, BIWAKO.temp_goal), 5)*1000
                     target_direction = math.radians(calculator.calculate_bearing(current_point, BIWAKO.temp_goal))
                     diff_deg =  math.degrees(calculator.limit_angle(target_direction - current_yaw))
-                    action = actions.omni_control_action(diff_deg, diff_distance)
-                    BIWAKO.cmd = action[0]
-                    BIWAKO.pwm = action[1]
-                    control_thruster(action)
+                    deg_e.append(diff_deg)
+                    if control_mode == 0:
+                        action = actions.omni_control_action(diff_deg, diff_distance)
+                        BIWAKO.cmd = action[0]
+                        BIWAKO.pwm = action[1]
+                        control_thruster(action)
+
+                    elif control_mode == 1:
+                        action = actions.diagonal_action(diff_deg, diff_distance, deg_e)
+                        control_thruster(action[0])
+                        control_thruster(action[1])
+                        BIWAKO.cmd = [action[0][0], action[1][0]]
+                        BIWAKO.pwm = [action[0][1], action[1][1]]
+
                     time.sleep(0.02)
 
-                    if abs(diff_distance) < temp_target_distance_torelance:
-                        break
-
-        if (state_data_log==True):
+        if (state_data_log is True):
             for i in range(len(log_data)):
                 csvWriter.writerow(log_data[i])
             file.close()
 
-        update_wt_thread.join()
+        if wt_log_mode is True:
+            update_wt_thread.join()
         master.arducopter_disarm()
         print("Arm/Disarm: Disarm")
         signal.signal(signal.SIGALRM, kill_signal_process)
         signal.setitimer(signal.ITIMER_REAL, 1, 0.5)
 
     except KeyboardInterrupt:
-        if (state_data_log==True):
+        if (state_data_log is True):
             for i in range(len(log_data)):
                 csvWriter.writerow(log_data[i])
             file.close()
 
-        update_wt_thread.join()
+        if wt_log_mode is True:
+            update_wt_thread.join()
+
         master.arducopter_disarm()
         print("Arm/Disarm: Disarm")
         time.sleep(3)
--- robot_control_action.py
+++ robot_control_action.py
@@ -14,12 +14,35 @@ def P_control(distance):
 
     return t_out
 
+def PD_heading_control(diff_deg, e):
+    # diff_deg; type: float, unit: [deg]
+    # e; type: object(list), unit:[deg]
+    Kp = const.degree_Kp
+    Kd = const.degree_Kd
+
+    MAX_PULSE = const.MAX_PULSE
+    MIN_PULSE = const.MIN_PULSE
+
+    ch = 4
+
+    diff_e = e[len(e)-2]-e[len(e)-1]
+
+    t_out = int(1500 + (Kp * diff_deg + Kd * diff_e * (1/0.02)))
+
+    if t_out < MIN_PULSE:
+        t_out = MIN_PULSE
+    elif t_out > MAX_PULSE:
+        t_out = MAX_PULSE
+
+    action = [ch, t_out]
+    return action
+
 def stay_action():
     ch = 4
     pwm = 1500
     action = [ch, pwm]
 
-    if const.debug_mode == True:
+    if const.debug_mode is True:
         print("Keep the position")
         print("########################")
     return action
@@ -47,17 +70,83 @@ def omni_control_action(diff_deg, diff_distance):
 
     action = [ch, pwm]
 
-    if const.debug_mode == True:
-        if cmd == 1:
-            print("FORWARD)
-        elif cmd == 2:
+    if const.debug_mode is True:
+        if cmd is 1:
+            print("FORWARD")
+        elif cmd is 2:
             print("BACKWARD")
-        elif cmd == 3:
+        elif cmd is 3:
             print("RIGHT")
-        elif cmd == 4:
+        elif cmd is 4:
             print("LEFT")
 
         print("pwm: ", pwm)
+        print("diff deg: ", diff_deg)
+        print("diff distance: ", diff_distance)
+
+    return action
+
+def diagonal_action(diff_deg, diff_distance, deg_e):
+    MAX_PULSE = const.MAX_PULSE
+    MIN_PULSE = const.MIN_PULSE
+    heading_torelance = const.heading_torelance
+
+    cmd = 0
+
+    dir_list = [i*1.0 for i in range(-180, 180+1, 45)]
+    # [0]:-180.0, [1]:-135.0, [2]:-90.0, [3]:-45.0,
+    # [4]:0.0, [5]:45.0, [6]:90.0, [7]:135.0, [8]:180.0
+    if dir_list[5]-heading_torelance < diff_deg < dir_list[5]+heading_torelance:
+        # forward first quadrant
+        action_1 = [5, 1679]
+        action_2 = [6, 1641]
+        action = [action_1, action_2]
+        cmd = 3
+    elif dir_list[3]-heading_torelance < diff_deg < dir_list[3]+heading_torelance:
+        # forward second quadrant
+        action_1 = [5, 1681]
+        action_2 = [6, 1666]
+        action = [action_1, action_2]
+        cmd = 4
+    elif dir_list[1]-heading_torelance < diff_deg < dir_list[1]+heading_torelance:
+        # forward third quadrant
+        action_1 = [5, 1348]
+        action_2 = [6, 1334]
+        action = [action_1, action_2]
+        cmd = 5
+    elif dir_list[7]-heading_torelance < diff_deg < dir_list[7]+heading_torelance:
+        # forward fourth quadrant
+        action_1 = [5, 1333]
+        action_2 = [6, 1643]
+        action = [action_1, action_2]
+        cmd = 6
+    else:
+        control_value = PD_heading_control(diff_deg, deg_e)
+        action = [control_value, control_value]
+        if action[0][1] is 1500:
+            cmd = 0
+        elif action[0][1] < 1500:
+            cmd = 1
+        elif action[0][1] > 1500:
+            cmd = 2
+
+    if const.debug_mode is True:
+        if cmd is 0:
+            print('Stop')
+        elif cmd is 1:
+            print('CW')
+        elif cmd is 2:
+            print('CCW')
+        elif cmd is 3:
+            print('First Quadrant')
+        elif cmd is 4:
+            print('Second Quadrant')
+        elif cmd is 5:
+            print('Third Quadrant')
+        elif cmd is 6:
+            print('Fourth Quadrant')
+
+        print("action: ", action)
         print("diff deg: ", diff_deg)
         print("diff distance: ", diff_distance)
 
