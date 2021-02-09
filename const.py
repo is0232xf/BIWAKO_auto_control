98606f3932c69d88b60ac38fbeed91e8b5e8e854
Yasuyuki Fujii <is0232xf@gmail.com>
2021-02-09 16:57:04 +0900

    [fix] add parameters

--- const.py
+++ const.py
@@ -9,14 +9,20 @@ class parameter:
 		self.MAX_PULSE = 1662
 		self.MIN_PULSE= 1362
 
-		# WAY point
-		self.way_point_file = './way_point/test.csv'
+		# WAY POINT
+		self.way_point_file = './way_point/target.csv'
+
 		# MODE
 		self.data_log_mode = True
-		self.debug_mode = True
+		self.debug_mode = False
+		self.wt_log_mode = False
+
+		#CONTROL MODE
+		self.control_mode = 0
+		# 0:OMNICONTROL MODE, 1:DIAGNALCONTROL MODE
 
 		# OTHER
 		self.main_target_distance_torelance = 3.0
 		self.temp_target_distance_torelance = 1.0
-		self.heading_torelance = 15.0
+		self.heading_torelance = 10.0
 		self.duration = 10.0
