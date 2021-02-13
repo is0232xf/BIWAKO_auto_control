d315458bffb0226e893331c1f02c0e51475ed6fd
Yasuyuki Fujii <is0232xf@gmail.com>
2021-02-13 18:17:22 +0900

    [add] new parameter

--- const.py
+++ const.py
@@ -4,13 +4,14 @@ class parameter:
 		# ROBOT CONTROL PARAMETER
 		self.distance_Kp = 100
 		self.distance_Kd = 50
-		self.degree_Kp = 1.8
+		self.degree_Kp = 1.3
 		self.degree_Kd = 0.8
-		self.MAX_PULSE = 1662
-		self.MIN_PULSE= 1362
+		self.MAX_PULSE = 1670
+		self.MIN_PULSE = 1330
+		self.PWM_OFFSET = 12
 
 		# WAY POINT
-		self.way_point_file = './way_point/target.csv'
+		self.way_point_file = './way_point/maiami_target.csv'
 
 		# MODE
 		self.data_log_mode = True
@@ -27,6 +28,6 @@ class parameter:
 
 		# OTHER
 		self.main_target_distance_torelance = 3.0
-		self.temp_target_distance_torelance = 1.0
-		self.heading_torelance = 10.0
+		self.temp_target_distance_torelance = 2.0
+		self.heading_torelance = 20.0
 		self.duration = 10.0
